9c77e247bc61d639a7da0a52310ea653dd9534f1
FazeDragon <74134721+FazeDragon@users.noreply.github.com>
2021-01-22 09:27:14 +0000

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,31 @@
+class Ball
+{
+    constructor(x,y,r)
+    {
+        var options= {
+            isStatic : false,
+            restitution : 0,
+            friction : 0.5,
+            density : 5
+        }
+        this.body = Bodies.circle(x,y,r,options);
+        this.radius = r;       
+        World.add(world , this.body);
+    }
+
+    display()
+    {
+        var pos= this.body.position;
+        var angle= this.body.angle; 
+        
+        push();
+        translate(pos.x,pos.y)
+        rotate(angle);
+        strokeWeight(1);
+        fill("yellow");
+        ellipseMode(CENTER);
+        ellipse(0,0,this.radius,this.radius);       
+        pop();
+
+    }
+}
---
+++ Box.js
@@ -0,0 +1,54 @@
+class Box
+{
+    constructor(x,y,w,h)
+    {
+        var options= {
+            isStatic : false,
+            restitution : 0.3,
+            friction : 0.5,
+            density : 1.2
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.width = w;
+        this.height= h;
+        this.image = loadImage("images/box.png");
+        World.add(world , this.body);
+        this.visibility = 255;
+    }
+
+    display()
+    {
+        var pos= this.body.position;
+        var angle= this.body.angle;
+       
+ 
+        // In case boxes fall , they would cover some distance in the left or right. 
+       if((pos.x < 200 || pos.x>750) && pos.y >570)
+       {            
+           World.remove(world, this.body);
+           push();
+           strokeWeight(0);
+           this.visibility = this.visibility - 20;
+           translate(pos.x,pos.y)
+           rotate(angle);            
+           tint(255, this.visibility);
+           image(this.image , 0,0 , 50,50);
+           pop();
+       }
+       // Regular display
+       else{
+            push(); 
+            strokeWeight(0);        
+             
+            translate(pos.x,pos.y)
+            rotate(angle);
+            rectMode(CENTER);
+            rect(0,0,this.width,this.height);
+            imageMode(CENTER);
+            image(this.image, 0,0, 50,50);            
+            pop();
+       }
+
+    }
+
+}
